6defa01a94d8acf89bfce0aa0a611fe7a625fbd4
honeysdet <honey.profile.crm@gmail.com>
2023-01-30 18:37:15 +0530

    webdriver updated

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -6,10 +6,10 @@ from Config.Config import TestData
 """global fixtures are defined over here"""
 @pytest.fixture(scope="class")
 def init_driver(request):
-    web_driver = webdriver.Chrome(executable_path=TestData.CHROME_EXECUTABLE_PATH)
+    web_driver = webdriver.Chrome()
     request.cls.driver = web_driver
     yield
     web_driver.quit()
 @pytest.fixture(scope="class")
 def input_data_for_add_user():
-    return["test_first","test_mid","test_last","test_username","Test@123456","test_first test_mid"]
+    return["test_first","test_mid","test_last","test_unique_id","Test@123456","test_first test_mid"]
